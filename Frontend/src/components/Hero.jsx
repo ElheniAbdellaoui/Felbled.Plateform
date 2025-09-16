e68407e3cfbad5e853d4ffb9b36c7c69ac1202e4
ElheniAbdellaoui <abedellaouihani@gmail.com>
2025-09-17 01:14:06 +0200

    first commit

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -2,30 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import heroImg from "../assets/blog.png";
+import { useTranslation } from "react-i18next";
 
 const Hero = () => {
+  const { t } = useTranslation();
   return (
     <div className="px-4 md:px-0">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center h-[600px] my-10 md:my-0">
         {/* text section  */}
         <div className="max-w-2xl">
           <h1 className="text-4xl md:text-6xl font-bold mb-4">
-            Investissez avec confiance dans vos finances
+            {t("hero.title")}
           </h1>
           <p className="text-lg md:text-xl opacity-80 mb-6">
-            Avec notre plateforme,prenez l’avantage avec des outils intuitifs et
-            des experts pour vous guider dans vos décisions d’invertissement
+            {t("hero.subtitle")}
           </p>
           <div className="flex space-x-4">
             <Link to={"/dashboard/write-blog"}>
-              <Button className="text-lg">Rejoinez-vous</Button>
+              <Button className="text-lg">{t("buttons.join")}</Button>
             </Link>
             <Link to={"/about"}>
               <Button
                 variant="outline"
                 className="border-white px-6 py-3 text-lg"
               >
-                Learn More
+                {t("buttons.learn_more")}
               </Button>
             </Link>
           </div>
