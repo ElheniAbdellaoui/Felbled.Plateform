d3eb6860101148f62b6f637ab21d28ac89740fec
ElheniAbdellaoui <abedellaouihani@gmail.com>
2025-09-18 02:17:14 +0200

    first commit

--- Frontend/src/components/PopularAuthors.jsx
+++ Frontend/src/components/PopularAuthors.jsx
@@ -9,6 +9,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
 
 const PopularAuthors = () => {
   const [popularUser, setPopularUser] = useState([]);
