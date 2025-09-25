00176a32d2cd3309c5f3c1526863e8444e277f69
ElheniAbdellaoui <abedellaouihani@gmail.com>
2025-09-25 21:46:05 +0200

    first commit

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,18 +1,28 @@
 import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // true pour 465, false pour 587
+      auth: {
+        user: process.env.EMAIL_USER, // stocke ton email dans .env
+        pass: process.env.EMAIL_PASS, // stocke ton app password Gmail dans .env
+      },
+    });
 
-  await transporter.sendMail({
-    from: `"Felbled Plateform" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    const info = await transporter.sendMail({
+      from: `"Felbled Plateform" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("📩 Email envoyé:", info.messageId);
+    return info;
+  } catch (error) {
+    console.error("❌ Erreur envoi email:", error);
+    throw new Error("Impossible d’envoyer l’email");
+  }
 };
